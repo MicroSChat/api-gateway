d123aade2959c9fecd69a0272271e62355ce4d54
Konstantin Buinak <kbuinak@gmail.com>
2021-03-14 21:05:03 +0100

    Initial commit

---
+++ src/main/java/com/microschat/mscapigateway/MscapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microschat.mscapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscapigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MscapigatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microschat/mscapigateway/MscapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microschat.mscapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscapigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
