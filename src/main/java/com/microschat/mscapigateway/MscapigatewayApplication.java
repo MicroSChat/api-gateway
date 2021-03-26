63902a0955b6a6379285d8f7325018049a79807a
Konstantin Buinak <kbuinak@gmail.com>
2021-03-26 20:08:43 +0100

    feat: moved config to common lib

--- src/main/java/com/microschat/mscapigateway/MscapigatewayApplication.java
+++ src/main/java/com/microschat/mscapigateway/MscapigatewayApplication.java
@@ -8,8 +8,10 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.microschat.*")
+@ComponentScan(basePackages = "com.microschat.*")
 public class MscapigatewayApplication {
 
     public static void main(String[] args) {
